0a9eee98fa4322d3b87837d1a1fa90517216bd72
senzhisenzhi <senzhisenzhi@gmail.com>
2024-12-02 11:16:31 +0800

    Added Button Script and Sprites

---
+++ TRINITY/Assets/C#/UI/ButtonFunction.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunction : MonoBehaviour
+{
+    public string menu = "MenuScene";
+    public string crabBossStage = "CrabStageBossScene";
+    public string flyingBossStage = "FlyingBossStageScene";
+    public string humanoidBossStage = "HumanoidBossStageScene";
+
+    public void OnMenu()
+    {
+        SceneManager.LoadScene(menu);
+    }public void OnCrabBoss()
+    {
+        SceneManager.LoadScene(crabBossStage);
+    }
+    public void OnFlyingBoss()
+    {
+        SceneManager.LoadScene(flyingBossStage);
+    }
+    public void OnHumanoid()
+    {
+        SceneManager.LoadScene(humanoidBossStage);
+    }
+    public void OnRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
