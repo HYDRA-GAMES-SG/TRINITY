dd2045a3087bb0dcfa334f269290bb6095d6a3fc
03_LamHauJin <minimx1999@gmail.com>
2025-02-05 22:02:56 +0800

    Turn on Invincible boss shot shock indecator

--- TRINITY/Assets/C#/InvincibleBoss/IBLongAttack_ShotShock.cs
+++ TRINITY/Assets/C#/InvincibleBoss/IBLongAttack_ShotShock.cs
@@ -14,11 +14,11 @@ public class IBLongAttack_ShotShock : InvincibleBossState
     [SerializeField] float ShockDelay;
     [SerializeField] float ShockSpeed;
 
-    [SerializeField] Transform Indicator;
-    [SerializeField] Vector3 targetScale;
-    [SerializeField] Vector3 initialScale;
-    [SerializeField] float scaleSpeed = 30f; // Z scale change per second
-    [SerializeField] bool isScaling = false;
+    //[SerializeField] Transform Indicator;
+    //[SerializeField] Vector3 targetScale;
+    //[SerializeField] Vector3 initialScale;
+    //[SerializeField] float scaleSpeed = 30f; // Z scale change per second
+    //[SerializeField] bool isScaling = false;
 
     float timer = 0;
     bool hasShot = false;
@@ -36,8 +36,8 @@ public class IBLongAttack_ShotShock : InvincibleBossState
 
         ShockBlue.Play();
 
-        initialScale = Indicator.localScale;
-        isScaling = false;
+        //initialScale = Indicator.localScale;
+        //isScaling = false;
     }
 
     public override void PreUpdateBehaviour(float dt)
@@ -66,23 +66,23 @@ public class IBLongAttack_ShotShock : InvincibleBossState
         ShockBluePos.transform.rotation = Quaternion.LookRotation(ShockBlueDirection, Vector3.up);
 
         timer += Time.fixedDeltaTime;
-        if (timer >= 2 && !isScaling)
-        {
-            isScaling = true;
-            Indicator.gameObject.SetActive(true);
-        }
-        if (isScaling)
-        {
-            Indicator.localScale = Vector3.Lerp(Indicator.localScale, targetScale, Time.deltaTime / (1f / scaleSpeed));
+        //if (timer >= 2 && !isScaling)
+        //{
+        //    isScaling = true;
+        //    Indicator.gameObject.SetActive(true);
+        //}
+        //if (isScaling)
+        //{
+        //    Indicator.localScale = Vector3.Lerp(Indicator.localScale, targetScale, Time.deltaTime / (1f / scaleSpeed));
 
-            if (Mathf.Abs(Indicator.localScale.z - targetScale.z) < 0.01f)
-            {
-                Indicator.localScale = targetScale;
-            }
-        }
+        //    if (Mathf.Abs(Indicator.localScale.z - targetScale.z) < 0.01f)
+        //    {
+        //        Indicator.localScale = targetScale;
+        //    }
+        //}
         if (timer >= ShockDelay && !hasShot)
         {
-            Indicator.gameObject.SetActive(false);
+            //Indicator.gameObject.SetActive(false);
             hasShot = true;
             IBShock shock = Instantiate(ShockTrail, ShockBluePos.transform.position, ShockBluePos.transform.rotation);
             shock.GetController(InvincibleBossFSM.InvincibleBossController);
@@ -100,9 +100,9 @@ public class IBLongAttack_ShotShock : InvincibleBossState
         hasShot = false;
         ShockBlue.Stop();
         InvincibleBossFSM.InvincibleBossController.bCanShotShock = false;
-        Indicator.localScale = initialScale;
-        Indicator.gameObject.SetActive(false);
-        isScaling = false;
+        //Indicator.localScale = initialScale;
+        //Indicator.gameObject.SetActive(false);
+        //isScaling = false;
 
     }
 
